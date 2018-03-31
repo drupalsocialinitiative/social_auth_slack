e48691a8c64489e6fed314513d6fd96af02eb6f6
Getulio Sánchez <valentin2507@gmail.com>
2018-03-30 23:19:36 -0400

    Issue #2957393: Add tests for settings form

---
+++ tests/src/Functional/SocialAuthSlackSettingsFormTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Drupal\Tests\social_auth_slack\Functional;
+
+use Drupal\social_api\SocialApiSettingsFormBaseTest;
+
+/**
+ * Test Social Auth Slack settings form functionality.
+ *
+ * @group social_auth
+ *
+ * @ingroup social_auth_slack
+ */
+class SocialAuthSlackSettingsFormTest extends SocialApiSettingsFormBaseTest {
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = ['social_auth_slack'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    $this->module = 'social_auth_slack';
+    $this->socialNetwork = 'slack';
+    $this->moduleType = 'social-auth';
+    parent::setUp();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function testIsAvailableInIntegrationList() {
+    $this->fields = ['client_id', 'client_secret'];
+    parent::testIsAvailableInIntegrationList();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function testSettingsFormSubmission() {
+    $this->edit = [
+      'client_id' => $this->randomString(10),
+      'client_secret' => $this->randomString(10),
+    ];
+    parent::testSettingsFormSubmission();
+  }
+
+}
