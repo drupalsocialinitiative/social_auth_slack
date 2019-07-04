72781f8657db72aa93a479878d6b4de5ccd9d2b7
Getulio Sánchez <valentin2507@gmail.com>
2019-07-03 20:48:28 -0400

    Issue #3065765: Call checkAuthError to check for authentication errors

--- src/Controller/SlackAuthController.php
+++ src/Controller/SlackAuthController.php
@@ -3,6 +3,7 @@
 namespace Drupal\social_auth_slack\Controller;
 
 use Drupal\Core\Messenger\MessengerInterface;
+use Drupal\Core\Render\RendererInterface;
 use Drupal\social_api\Plugin\NetworkManager;
 use Drupal\social_auth\Controller\OAuth2ControllerBase;
 use Drupal\social_auth\SocialAuthDataHandler;
@@ -31,15 +32,20 @@ class SlackAuthController extends OAuth2ControllerBase {
    *   Used to access GET parameters.
    * @param \Drupal\social_auth\SocialAuthDataHandler $data_handler
    *   The Social Auth data handler.
+   * @param \Drupal\Core\Render\RendererInterface $renderer
+   *   Used to handle metadata for redirection to authentication URL.
    */
   public function __construct(MessengerInterface $messenger,
                               NetworkManager $network_manager,
                               UserAuthenticator $user_authenticator,
                               SlackAuthManager $slack_manager,
                               RequestStack $request,
-                              SocialAuthDataHandler $data_handler) {
+                              SocialAuthDataHandler $data_handler,
+                              RendererInterface $renderer) {
 
-    parent::__construct('Social Auth Slack', 'social_auth_slack', $messenger, $network_manager, $user_authenticator, $slack_manager, $request, $data_handler);
+    parent::__construct('Social Auth Slack', 'social_auth_slack', $messenger,
+                         $network_manager, $user_authenticator, $slack_manager,
+                         $request, $data_handler, $renderer);
   }
 
   /**
@@ -52,7 +58,8 @@ class SlackAuthController extends OAuth2ControllerBase {
       $container->get('social_auth.user_authenticator'),
       $container->get('social_auth_slack.manager'),
       $container->get('request_stack'),
-      $container->get('social_auth.data_handler')
+      $container->get('social_auth.data_handler'),
+      $container->get('renderer')
     );
   }
 
@@ -63,11 +70,10 @@ class SlackAuthController extends OAuth2ControllerBase {
    */
   public function callback() {
 
-    // Checks if authentication failed.
-    if ($this->request->getCurrentRequest()->query->has('error')) {
-      $this->messenger->addError($this->t('You could not be authenticated.'));
-
-      return $this->redirect('user.login');
+    // Checks if there was an authentication error.
+    $redirect = $this->checkAuthError();
+    if ($redirect) {
+      return $redirect;
     }
 
     /* @var \AdamPaterson\OAuth2\Client\Provider\SlackResourceOwner|null $profile */
@@ -79,7 +85,12 @@ class SlackAuthController extends OAuth2ControllerBase {
       // Gets (or not) extra initial data.
       $data = $this->userAuthenticator->checkProviderIsAssociated($profile->getId()) ? NULL : $this->providerManager->getExtraDetails();
 
-      return $this->userAuthenticator->authenticateUser($profile->getName(), $profile->getEmail(), $profile->getId(), $this->providerManager->getAccessToken(), $profile->getImage192(), $data);
+      return $this->userAuthenticator->authenticateUser($profile->getName(),
+                                                        $profile->getEmail(),
+                                                        $profile->getId(),
+                                                        $this->providerManager->getAccessToken(),
+                                                        $profile->getImage192(),
+                                                        $data);
     }
 
     return $this->redirect('user.login');
